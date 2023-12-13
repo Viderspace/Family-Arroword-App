163503cfa253d84ce77b7f67fca99b94dfe0f1dd
Viderspace <50073459+Viderspace@users.noreply.github.com>
2023-12-13 21:12:20 +0200

    Initial Commit

---
+++ ArrowordApp/ArrowordAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArrowordAppApp.swift
+//  ArrowordApp
+//
+//  Created by Jonatan Vider on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ArrowordAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
